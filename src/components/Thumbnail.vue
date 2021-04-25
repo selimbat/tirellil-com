fb52dc48f8499eb03a0eabf280b3c4ce8b1e688f
selimbat <selim.tirellil@student.ecp.fr>
2021-04-25 13:03:07 +0200

    Add thumbnail component

---
+++ src/components/Thumbnail.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="wrapper">
+    <router-link class="thumbnail" :to="metadata.route">
+      <img :src="imgUrl(metadata.imgSrc)" :alt="metadata.imgAlt" />
+      <div class="textArea">
+        <p>{{ tagsStr }}</p>
+        <h5>{{ metadata.title }}</h5>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+var images = require.context(
+  "../assets/project-thumbnails/",
+  false,
+  /\.(png|jpg)$/
+);
+
+export default {
+  name: "Thumbnail",
+  props: {
+    metadata: {
+      type: Object,
+      //required: true,
+      default: function () {
+        return {
+          route: "#",
+          imgSrc: "3d-engine.png",
+          imgAlt: "A Utah teapot",
+          techTags: ["C++, OpenGL"],
+          title: "A dummy project",
+        };
+      },
+    },
+  },
+  computed: {
+    tagsStr: function () {
+      return this.metadata.techTags
+        .reduce((agg, tag) => agg + ", " + tag)
+        .toUpperCase();
+    },
+  },
+  methods: {
+    imgUrl: function (path) {
+      return images("./" + path);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  max-width: 400px;
+  aspect-ratio: 16/9;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  border-radius: 0.25rem;
+  margin: 0.5rem 0.75rem;
+}
+
+.thumbnail {
+  width: 100%;
+  height: 100%;
+  display: inline-block;
+  position: relative;
+}
+
+img {
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: -1;
+}
+.textArea {
+  position: absolute;
+  display: block;
+  background-color: antiquewhite;
+  opacity: 0;
+  border-radius: 0.25rem;
+  bottom: 1rem;
+  left: 1rem;
+  right: 1rem;
+  padding: 1rem 0;
+  transition: opacity 0.3s linear;
+}
+.thumbnail:hover,
+.thumbnail:focus {
+  .textArea {
+    opacity: 0.8;
+  }
+}
+
+.thumbnail:focus {
+  .textArea {
+    outline: black solid 1px;
+  }
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,16 +4,19 @@
       <h2>My projects</h2>
       <p>You can browse through all my recent projects</p>
     </Banner>
+    <Thumbnail> </Thumbnail>
   </div>
 </template>
 
 <script>
 import Banner from "@/components/Banner.vue";
+import Thumbnail from "@/components/Thumbnail.vue";
 
 export default {
   name: "Projects",
   components: {
     Banner,
+    Thumbnail,
   },
 };
 </script>
