d9c91a9f8b780807cbe1967fe1d7538fa2f6b553
selimbat <selim.tirellil@student.ecp.fr>
2021-04-28 19:47:27 +0200

    Add dynamic routing to projects

--- src/router/index.js
+++ src/router/index.js
@@ -16,17 +16,22 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   },
   {
     path: '/projects',
     name: 'Projects',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Projects.vue')
+    component: () => import('../views/Projects.vue')
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
+    component: () => import('../views/Contact.vue')
+  },
+  {
+    path: '/projects/:title',
+    name: 'Project',
+    component: () => import('../views/Project.vue')
   }
 ]
 
