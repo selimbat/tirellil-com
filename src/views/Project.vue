a59373a89931071be95fffe77353dbb4ee61a1c3
selimbat <selim.tirellil@student.ecp.fr>
2021-04-28 19:47:55 +0200

    Add the project view component

---
+++ src/views/Project.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="project">
+    <Banner>
+      <h2>{{ $route.params.title }}</h2>
+      <p>Is this working?</p>
+    </Banner>
+  </div>
+</template>
+
+<script>
+import Banner from "@/components/Banner.vue";
+
+export default {
+  name: "Projects",
+  components: {
+    Banner,
+  },
+};
+</script>
+
+<style scoped>
+</style>
