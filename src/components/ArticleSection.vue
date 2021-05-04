02d62ccda5c9de447754647d30b17f87530973be
selimbat <selim.tirellil@student.ecp.fr>
2021-05-05 00:02:11 +0200

    Add link support for articles

--- src/components/ArticleSection.vue
+++ src/components/ArticleSection.vue
@@ -84,18 +84,50 @@ export default {
     },
     beautifyText: function (text) {
       const boldSymbol = "**";
-      let boldOpeningIndex = text.indexOf(boldSymbol);
-      if (boldOpeningIndex < 0) {
-        return text;
+      const boldTags = { opening: "<strong>", closing: "</strong>" };
+      const linkSymbol = "~~";
+      const linkTags = { opening: (url) => `<a href="${url}">`, closing: "</a>"};
+
+      let boldIndex = { opening: text.indexOf(boldSymbol), closing: -1 };
+      let linkIndex = { opening: text.indexOf(linkSymbol), closing: -1 };
+
+      let ctr = 0;
+      while (boldIndex.opening >= 0 || linkIndex.opening >= 0) {
+        if (boldIndex.opening >= 0) {
+          boldIndex.closing = text.indexOf(boldSymbol, boldIndex.opening + boldSymbol.length);
+        }
+        if (linkIndex.opening >= 0) {
+          linkIndex.closing = text.indexOf(linkSymbol, linkIndex.opening + linkSymbol.length);
+        }
+        if ((boldIndex.opening - linkIndex.opening) * (boldIndex.closing - linkIndex.opening) * (boldIndex.opening - linkIndex.closing) * (boldIndex.closing - linkIndex.closing) < 0){
+          // tags intersect. Not supported
+          return "<span style=\"color: red\">Error when formatting text. Bold and link tags intersect.</span>"
+        }
+        
+        if (boldIndex.closing > 0 && (linkIndex.closing < 0 || boldIndex.closing < linkIndex.closing)) {
+          text = text.replace(boldSymbol, boldTags.opening).replace(boldSymbol, boldTags.closing);
+          boldIndex.opening = text.indexOf(boldSymbol);
+          boldIndex.closing = -1;
+        }
+        else if (linkIndex.closing > 0 && (boldIndex.closing < 0 || linkIndex.closing < boldIndex.closing)) {
+          let urlStart = text.indexOf("[", linkIndex.opening + linkSymbol.length);
+          let urlEnd = -1;
+          let url = "#";
+          if (urlStart >= 0){
+            urlEnd = text.indexOf("]", urlStart);
+            if (urlEnd >= 0){
+              url = text.substring(urlStart + 1, urlEnd);
+              text = text.replace(`[${url}]`, "");
+            }
+          }
+          text = text.replace(linkSymbol, linkTags.opening(url)).replace(linkSymbol, linkTags.closing);
+          linkIndex.opening = text.indexOf(linkSymbol);
+          linkIndex.closing = -1;
+        }
+        if (ctr > 50) break; // should never happen
+        ctr++;
       }
-      let boldClosingIndex = text.indexOf(boldSymbol, boldOpeningIndex + 2);
-      if (boldClosingIndex < 0) {
-        return text.replace(boldSymbol, "");
-      }
-      let treated = text
-        .replace(boldSymbol, "<strong>")
-        .replace(boldSymbol, "</strong>");
-      return this.beautifyText(treated);
+      return text;
     },
   },
 };
