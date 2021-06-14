7ae90ad8c3ddb6e0dc42034d441515313bbd3a63
selimbat <selim.tirellil@student.ecp.fr>
2021-06-14 19:19:10 +0200

    Add mobile support for interative sub-sections

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,7 +1,9 @@
 <template>
-  <ResponsiveContainer>
+  <ResponsiveContainer @toggle-mobile-view="toggleMobileView">
     <div class="repos-wrapper" v-if="repository">
-      <a class="repos" target="_blank" :href="repository.url">{{ hostLabel }}</a>
+      <a class="repos" target="_blank" :href="repository.url">{{
+        hostLabel
+      }}</a>
     </div>
 
     <article class="article">
@@ -9,69 +11,81 @@
         v-for="(section, index) in sections"
         :key="index"
         :section="section"
+        :mobileView="mobileView"
       ></ArticleSection>
     </article>
   </ResponsiveContainer>
 </template>
 
 <script>
-import ArticleSection from "@/components/ArticleSection.vue";
-import ResponsiveContainer from "@/components/ResponsiveContainer.vue";
+  import ArticleSection from "@/components/ArticleSection.vue";
+  import ResponsiveContainer from "@/components/ResponsiveContainer.vue";
 
-export default {
-  name: "Article",
-  components: {
-    ArticleSection,
-    ResponsiveContainer,
-  },
-  props: {
-    sections: {
-      type: Array,
-      required: true,
+  export default {
+    name: "Article",
+    components: {
+      ArticleSection,
+      ResponsiveContainer,
     },
-    repository: {
-      type: Object
-    }
-  },
-  computed:{
-    hostLabel: function (){
-      if (this.repository && this.repository.host) {
-        return this.repository.host.substring(0, 1).toUpperCase() + this.repository.host.substring(1);
-      }
-      return "error";
-    }
-  }
-};
+    props: {
+      sections: {
+        type: Array,
+        required: true,
+      },
+      repository: {
+        type: Object,
+      },
+    },
+    data() {
+      return {
+        mobileView: false,
+      };
+    },
+    computed: {
+      hostLabel: function() {
+        if (this.repository && this.repository.host) {
+          return (
+            this.repository.host.substring(0, 1).toUpperCase() +
+            this.repository.host.substring(1)
+          );
+        }
+        return "error";
+      },
+    },
+    methods: {
+      toggleMobileView(mobileView) {
+        this.mobileView = mobileView;
+      },
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-
-.repos-wrapper {
-  display:inline-block;
-  padding: 2rem 0 1rem 0;
-  transition: transform 0.1s linear;
-  &:hover {
-  transform: translate(0, -8px);
-    .repos {
-      box-shadow: 0px 8px #444;
+  .repos-wrapper {
+    display: inline-block;
+    padding: 2rem 0 1rem 0;
+    transition: transform 0.1s linear;
+    &:hover {
+      transform: translate(0, -8px);
+      .repos {
+        box-shadow: 0px 8px #444;
+      }
     }
   }
-}
 
-.repos {
-  text-decoration:none;
-  font-size: 2rem;
-  background-color: black;
-  color:white;
-  padding: 1rem 1rem;
-  border-radius: 0.6rem;
-  box-shadow: 0px 0px #444;
-  transition: box-shadow 0.1s linear;
-}
+  .repos {
+    text-decoration: none;
+    font-size: 2rem;
+    background-color: black;
+    color: white;
+    padding: 1rem 1rem;
+    border-radius: 0.6rem;
+    box-shadow: 0px 0px #444;
+    transition: box-shadow 0.1s linear;
+  }
 
-article {
-  padding: 0 8%;
-  margin-bottom: 3em;
-}
-
+  article {
+    padding: 0 8%;
+    margin-bottom: 3em;
+  }
 </style>
--- src/components/ArticleSection.vue
+++ src/components/ArticleSection.vue
@@ -10,204 +10,245 @@
         />
         <p class="img-annotation">{{ section.content.annotation }}</p>
       </div>
-      <div class="video-wrapper">
-        <div class="video-container" v-if="isVideo">
+      <div class="video-wrapper" v-if="isVideo">
+        <div class="video-container">
           <div class="video">
             <iframe :src="section.video.src" allowfullscreen=""></iframe>
           </div>
         </div>
       </div>
-      <div class="sub-sections-container" v-if="isMultiple">
+      <div
+        class="sub-sections-container"
+        :class="{ mobile: mobileView }"
+        v-if="isMultiple"
+      >
         <ArticleSection
           v-for="(subSection, index) in section.sections"
           :key="index"
           :section="subSection"
+          :mobileView="mobileView"
           :depth="depth + 1"
         ></ArticleSection>
       </div>
     </div>
     <div class="article-section" v-else>
-      <span class="error">
-        Section nesting is too deep ({{ depth }}).
-      </span>
+      <span class="error"> Section nesting is too deep ({{ depth }}). </span>
     </div>
   </section>
 </template>
 
 <script>
-const maxSupportedDepth = 2; // included
-var images = require.context("../assets/projects/", false, /\.(png|jpg)$/);
+  const maxSupportedDepth = 2; // included
+  var images = require.context("../assets/projects/", false, /\.(png|jpg)$/);
 
-export default {
-  name: "ArticleSection",
-  props: {
-    section: {
-      type: Object,
-      required: true,
+  export default {
+    name: "ArticleSection",
+    props: {
+      section: {
+        type: Object,
+        required: true,
+      },
+      depth: {
+        type: Number,
+        default: 0,
+      },
+      mobileView: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
     },
-    depth: {
-      type: Number,
-      default: 0,
+    computed: {
+      hasTitle: function() {
+        return this.section && "title" in this.section;
+      },
+      isParagraph: function() {
+        return (
+          this.section &&
+          this.section.type &&
+          (this.section.type === "paragraph" || this.section.type === "p")
+        );
+      },
+      isImage: function() {
+        return (
+          this.section &&
+          this.section.type &&
+          (this.section.type === "image" || this.section.type === "img")
+        );
+      },
+      isVideo: function() {
+        return (
+          this.section && this.section.type && this.section.type === "video"
+        );
+      },
+      isMultiple: function() {
+        return (
+          this.section &&
+          this.section.type &&
+          (this.section.type === "multiple" || this.section.type === "many") &&
+          this.section.sections &&
+          this.section.sections.length > 0
+        );
+      },
+      isNestingTooDeep: function() {
+        return this.depth > maxSupportedDepth;
+      },
     },
-  },
-  computed: {
-    hasTitle: function () {
-      return this.section && "title" in this.section;
-    },
-    isParagraph: function () {
-      return (
-        this.section &&
-        this.section.type &&
-        (this.section.type === "paragraph" || this.section.type === "p")
-      );
-    },
-    isImage: function () {
-      return (
-        this.section &&
-        this.section.type &&
-        (this.section.type === "image" || this.section.type === "img")
-      );
-    },
-    isVideo: function () {
-      return (
-        this.section &&
-        this.section.type &&
-        this.section.type === "video"
-      );
-    },
-    isMultiple: function () {
-      return (
-        this.section &&
-        this.section.type &&
-        (this.section.type === "multiple" || this.section.type === "many") &&
-        this.section.sections &&
-        this.section.sections.length > 0
-      );
-    },
-    isNestingTooDeep: function () {
-      return this.depth > maxSupportedDepth;
-    },
-  },
-  methods: {
-    imgUrl: function (path) {
-      if (images.keys().indexOf("./" + path) >= 0) {
-        return images("./" + path);
-      } else {
-        return images("./dodeca1.png"); // TODO: change to a real error image
-      }
-    },
-    beautifyText: function (text) {
-      const boldSymbol = "**";
-      const boldTags = { opening: "<strong>", closing: "</strong>" };
-      const linkSymbol = "~~";
-      const linkTags = { opening: (url) => `<a target="_blank" href="${url}">`, closing: "</a>"};
+    methods: {
+      imgUrl: function(path) {
+        if (images.keys().indexOf("./" + path) >= 0) {
+          return images("./" + path);
+        } else {
+          return images("./dodeca1.png"); // TODO: change to a real error image
+        }
+      },
+      beautifyText: function(text) {
+        const boldSymbol = "**";
+        const boldTags = { opening: "<strong>", closing: "</strong>" };
+        const linkSymbol = "~~";
+        const linkTags = {
+          opening: (url) => `<a target="_blank" href="${url}">`,
+          closing: "</a>",
+        };
 
-      let boldIndex = { opening: text.indexOf(boldSymbol), closing: -1 };
-      let linkIndex = { opening: text.indexOf(linkSymbol), closing: -1 };
+        let boldIndex = { opening: text.indexOf(boldSymbol), closing: -1 };
+        let linkIndex = { opening: text.indexOf(linkSymbol), closing: -1 };
 
-      let ctr = 0;
-      while (boldIndex.opening >= 0 || linkIndex.opening >= 0) {
-        if (boldIndex.opening >= 0) {
-          boldIndex.opening = text.indexOf(boldSymbol);
-          boldIndex.closing = text.indexOf(boldSymbol, boldIndex.opening + boldSymbol.length);
-        }
-        if (linkIndex.opening >= 0) {
-          linkIndex.opening = text.indexOf(linkSymbol);
-          linkIndex.closing = text.indexOf(linkSymbol, linkIndex.opening + linkSymbol.length);
-        }
-        if ((boldIndex.opening - linkIndex.opening) * (boldIndex.closing - linkIndex.opening) * (boldIndex.opening - linkIndex.closing) * (boldIndex.closing - linkIndex.closing) < 0){
-          // tags intersect. Not supported
-          return "<span style=\"error\">Error when formatting text. Bold and link tags intersect.</span>"
-        }
-        
-        if (boldIndex.closing > 0 && (linkIndex.closing < 0 || boldIndex.closing < linkIndex.closing)) {
-          text = text.replace(boldSymbol, boldTags.opening).replace(boldSymbol, boldTags.closing);
-          boldIndex.opening = text.indexOf(boldSymbol);
-          boldIndex.closing = -1;
-        }
-        else if (linkIndex.closing > 0 && (boldIndex.closing < 0 || linkIndex.closing < boldIndex.closing)) {
-          let urlStart = text.indexOf("[", linkIndex.opening + linkSymbol.length);
-          let urlEnd = -1;
-          let url = "#";
-          if (urlStart >= 0 && urlStart < linkIndex.closing) {
-            urlEnd = text.indexOf("]", urlStart);
-            if (urlEnd >= 0 && urlEnd < linkIndex.closing) {
-              url = text.substring(urlStart + 1, urlEnd);
-              text = text.replace(`[${url}]`, "");
-            }
+        let ctr = 0;
+        while (boldIndex.opening >= 0 || linkIndex.opening >= 0) {
+          if (boldIndex.opening >= 0) {
+            boldIndex.opening = text.indexOf(boldSymbol);
+            boldIndex.closing = text.indexOf(
+              boldSymbol,
+              boldIndex.opening + boldSymbol.length
+            );
           }
-          text = text.replace(linkSymbol, linkTags.opening(url)).replace(linkSymbol, linkTags.closing);
-          linkIndex.opening = text.indexOf(linkSymbol);
-          linkIndex.closing = -1;
+          if (linkIndex.opening >= 0) {
+            linkIndex.opening = text.indexOf(linkSymbol);
+            linkIndex.closing = text.indexOf(
+              linkSymbol,
+              linkIndex.opening + linkSymbol.length
+            );
+          }
+          if (
+            (boldIndex.opening - linkIndex.opening) *
+              (boldIndex.closing - linkIndex.opening) *
+              (boldIndex.opening - linkIndex.closing) *
+              (boldIndex.closing - linkIndex.closing) <
+            0
+          ) {
+            // tags intersect. Not supported
+            return '<span style="error">Error when formatting text. Bold and link tags intersect.</span>';
+          }
+
+          if (
+            boldIndex.closing > 0 &&
+            (linkIndex.closing < 0 || boldIndex.closing < linkIndex.closing)
+          ) {
+            text = text
+              .replace(boldSymbol, boldTags.opening)
+              .replace(boldSymbol, boldTags.closing);
+            boldIndex.opening = text.indexOf(boldSymbol);
+            boldIndex.closing = -1;
+          } else if (
+            linkIndex.closing > 0 &&
+            (boldIndex.closing < 0 || linkIndex.closing < boldIndex.closing)
+          ) {
+            let urlStart = text.indexOf(
+              "[",
+              linkIndex.opening + linkSymbol.length
+            );
+            let urlEnd = -1;
+            let url = "#";
+            if (urlStart >= 0 && urlStart < linkIndex.closing) {
+              urlEnd = text.indexOf("]", urlStart);
+              if (urlEnd >= 0 && urlEnd < linkIndex.closing) {
+                url = text.substring(urlStart + 1, urlEnd);
+                text = text.replace(`[${url}]`, "");
+              }
+            }
+            text = text
+              .replace(linkSymbol, linkTags.opening(url))
+              .replace(linkSymbol, linkTags.closing);
+            linkIndex.opening = text.indexOf(linkSymbol);
+            linkIndex.closing = -1;
+          }
+          if (ctr > 50) break; // should never happen
+          ctr++;
         }
-        if (ctr > 50) break; // should never happen
-        ctr++;
-      }
-      return text;
+        return text;
+      },
     },
-  },
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-.article-section {
-  text-align: left;
-}
-.article-section > h5,
-.article-section > p,
-.article-section > div {
-  margin: 1rem 0;
-}
+  .article-section {
+    text-align: left;
+  }
+  .article-section > h5,
+  .article-section > p,
+  .article-section > div {
+    margin: 1.3rem 0;
+  }
 
-.article-section > h5 {
-  text-transform: uppercase;
-  font-weight: 600;
-}
+  section:not(:first-of-type) > .article-section > h5 {
+    margin-top: 2.5rem;
+  }
 
-.sub-sections-container {
-  display: flex;
-}
+  .article-section > h5 {
+    text-transform: uppercase;
+    font-weight: 600;
+  }
 
-.sub-sections-container > * {
-  width: 100%;
-  margin: 0 0.5rem 0 0.5rem;
-}
+  .sub-sections-container {
+    display: flex;
+  }
 
-.img-container {
-  text-align: center;
-}
+  .sub-sections-container.mobile {
+    flex-direction: column;
+  }
 
-.img-container > img {
-  max-width: 100%;
-}
+  .sub-sections-container > * {
+    width: 100%;
+    margin: 0 0.5rem 0 0.5rem;
+  }
 
-.img-annotation {
-  font-style: italic;
-}
+  .img-container {
+    text-align: center;
+  }
 
-.video-wrapper {
-  display:flex;
-  justify-content: center;
-}
+  .img-container > img {
+    max-width: 100%;
+  }
 
-.video-container {
-  flex-basis: 700px;
-}
+  .img-annotation {
+    font-style: italic;
+  }
 
-.video {
-  position:relative;
-  padding-top: calc(100% / (16 / 9));
-}
+  .video-wrapper {
+    display: flex;
+    justify-content: center;
+  }
 
-.video > iframe {
-  position:absolute;
-  width:100%;
-  height:100%;
-  top:0;
-  left:0;
-}
+  .video-container {
+    flex-basis: 700px;
+  }
 
-.error {
-  color: red;
-}
+  .video {
+    position: relative;
+    padding-top: calc(100% / (16 / 9));
+  }
+
+  .video > iframe {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+
+  .error {
+    color: red;
+  }
 </style>
