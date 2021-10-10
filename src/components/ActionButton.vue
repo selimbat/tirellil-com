f62982ed322d4772d3f5a30663a517517a3013b2
selimbat <selim.tirellil@student.ecp.fr>
2021-10-10 18:43:09 +0200

    add contact actions

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="action-wrapper">
+    <a @click="callback()">
+      {{ label }}
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ActionButton",
+    props: {
+      label: {
+        type: String,
+        required: true,
+      },
+      callback: {
+        type: Function,
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .action-wrapper {
+    margin-top: 3em;
+    margin-bottom: 3em;
+  }
+  a {
+    color: var(--accent-color);
+    display: inline-block;
+    width: 20ch;
+    cursor: pointer;
+    padding: 1em 2em;
+    border-radius: 11% / 50%;
+    border: 2px solid var(--accent-color);
+    transition: all 0.3s;
+    &:hover {
+      color: white;
+      background-color: var(--accent-color);
+    }
+  }
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,16 +4,32 @@
       <h2>Get in touch</h2>
       <p>Tell me about your project</p>
     </Banner>
+    <section>
+      <ActionButton label="Download my resume" :callback="downloadResume" />
+      <ActionButton label="Email me" :callback="openMailClient" />
+    </section>
   </div>
 </template>
 
 <script>
-import Banner from "@/components/Banner.vue";
+  import Banner from "@/components/Banner.vue";
+  import ActionButton from "@/components/ActionButton.vue";
 
-export default {
-  name: "Contact",
-  components: {
-    Banner,
-  },
-};
+  export default {
+    name: "Contact",
+    components: {
+      Banner,
+      ActionButton,
+    },
+    methods: {
+      openMailClient() {
+        window.open(
+          "mailto:selim.tirellil@hotmail.fr?subject=Contact from tirellil.com"
+        );
+      },
+      downloadResume() {
+        window.open("/resume.pdf", "resizable,scrollbars");
+      },
+    },
+  };
 </script>
