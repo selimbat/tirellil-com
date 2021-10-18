bf89ca7d01ef8d038807dc40e79db2d04735c3d0
selimbat <selim.tirellil@student.ecp.fr>
2021-10-18 22:17:55 +0200

    Move converter method to different file

--- src/components/ArticleSection.vue
+++ src/components/ArticleSection.vue
@@ -38,6 +38,7 @@
 </template>
 
 <script>
+  import { beautifyText } from "@/services/syntaxParser.js";
   const maxSupportedDepth = 2; // included
   var images = require.context("../assets/projects/", false, /\.(png|jpg)$/);
 
@@ -103,81 +104,7 @@
         }
       },
       beautifyText: function(text) {
-        // TODO: format with regex and put in different file
-        const boldSymbol = "**";
-        const boldTags = { opening: "<strong>", closing: "</strong>" };
-        const linkSymbol = "~~";
-        const linkTags = {
-          opening: (url) => `<a rel="external" target="_blank" href="${url}">`,
-          closing: "</a>",
-        };
-
-        let boldIndex = { opening: text.indexOf(boldSymbol), closing: -1 };
-        let linkIndex = { opening: text.indexOf(linkSymbol), closing: -1 };
-
-        let ctr = 0;
-        while (boldIndex.opening >= 0 || linkIndex.opening >= 0) {
-          if (boldIndex.opening >= 0) {
-            boldIndex.opening = text.indexOf(boldSymbol);
-            boldIndex.closing = text.indexOf(
-              boldSymbol,
-              boldIndex.opening + boldSymbol.length
-            );
-          }
-          if (linkIndex.opening >= 0) {
-            linkIndex.opening = text.indexOf(linkSymbol);
-            linkIndex.closing = text.indexOf(
-              linkSymbol,
-              linkIndex.opening + linkSymbol.length
-            );
-          }
-          if (
-            (boldIndex.opening - linkIndex.opening) *
-              (boldIndex.closing - linkIndex.opening) *
-              (boldIndex.opening - linkIndex.closing) *
-              (boldIndex.closing - linkIndex.closing) <
-            0
-          ) {
-            // tags intersect. Not supported
-            return '<span style="error">Error when formatting text. Bold and link tags intersect.</span>';
-          }
-
-          if (
-            boldIndex.closing > 0 &&
-            (linkIndex.closing < 0 || boldIndex.closing < linkIndex.closing)
-          ) {
-            text = text
-              .replace(boldSymbol, boldTags.opening)
-              .replace(boldSymbol, boldTags.closing);
-            boldIndex.opening = text.indexOf(boldSymbol);
-            boldIndex.closing = -1;
-          } else if (
-            linkIndex.closing > 0 &&
-            (boldIndex.closing < 0 || linkIndex.closing < boldIndex.closing)
-          ) {
-            let urlStart = text.indexOf(
-              "[",
-              linkIndex.opening + linkSymbol.length
-            );
-            let urlEnd = -1;
-            let url = "#";
-            if (urlStart >= 0 && urlStart < linkIndex.closing) {
-              urlEnd = text.indexOf("]", urlStart);
-              if (urlEnd >= 0 && urlEnd < linkIndex.closing) {
-                url = text.substring(urlStart + 1, urlEnd);
-                text = text.replace(`[${url}]`, "");
-              }
-            }
-            text = text
-              .replace(linkSymbol, linkTags.opening(url))
-              .replace(linkSymbol, linkTags.closing);
-            linkIndex.opening = text.indexOf(linkSymbol);
-            linkIndex.closing = -1;
-          }
-          if (ctr > 50) break; // should never happen
-          ctr++;
-        }
-        return text;
+        return beautifyText(text);
       },
     },
   };
